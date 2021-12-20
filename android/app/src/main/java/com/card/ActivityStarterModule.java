295056151dfcc13c9602948161cd6b6d13f421f4
saisuresh388 <75465586+saisuresh-banc91@users.noreply.github.com>
2021-12-20 16:43:56 +0530

    Building ith new build

--- android/app/src/main/java/com/card/ActivityStarterModule.java
+++ android/app/src/main/java/com/card/ActivityStarterModule.java
@@ -16,7 +16,13 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.card91.samplecalculator.*;
+// import com.card91.samplecalculator.*;
+//import io.card91.android.sdk.ui.new_flow_v1.*;
+//import io.card91.android.sdk.*;
+import io.card91.android.sdk.ui.verification.C91VerifyMobileScreen;
+import io.card91.android.sdk.utils.C91Constant;
+import io.card91.android.sdk.utils.SdkEnv;
+
 import com.facebook.react.bridge.WritableNativeArray;
 class ActivityStarterModule extends ReactContextBaseJavaModule {
 
@@ -37,8 +43,13 @@ class ActivityStarterModule extends ReactContextBaseJavaModule {
     void navigateToExample() {
         try {
             ReactApplicationContext context = getReactApplicationContext();
-            Intent intent = new Intent(context, CalculatorActivity.class);
+            // Intent intent = new Intent(context, CalculatorActivity.class);
+            // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            Intent intent = new Intent(context, C91VerifyMobileScreen.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(C91Constant.ENVIRONMENT, SdkEnv.DEV);
+            intent.putExtra(C91Constant.NUMBER, "919876543213");
+            intent.putExtra(C91Constant.DEVICE_ID, "uiyrdswe218973");
             context.startActivity(intent);
         } catch (Exception e) {
             e.printStackTrace();
